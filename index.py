b28b1008c62bd55f2615b2a767e69fdec810fb9b
Christian Williams <chrisawill05@gmail.com>
2019-08-28 12:57:06 -0500

    Completed assignment

---
+++ index.py
@@ -0,0 +1,63 @@
+
+# ### Problem 1:
+
+# def problem1():
+#     class Dog:
+#         def __init__(self, name, breed, color, gender):
+#             self.name = name
+#             self.breed = breed
+#             self.color = color
+#             self.gender = gender
+#
+#         def pupper(self):
+#             print(f"The dog {self.name} is a {self.breed}, who has a {self.color} coat and is {self.gender} years old.")
+#
+#     adoption = Dog("Rose", "Pitbull", "white and brown", "4")
+#
+#     print(adoption.pupper)
+
+
+# ### Problem 2:
+#
+# def problem2():
+#     quit2 = input("Enter =. ")
+#     while quit2 != "=":
+#         quit2 = input("Try again- ")
+
+
+# ### Problem 3:
+# Create a class Product that represents a product sold online. A product has a name, price, and quantity.
+#
+# The class should have several functions:
+#
+# a) One function that can change the name of the product.
+#
+#     b) Another function that can change the name and price of the product.
+#
+#     c) A last function that can change the name, price, and quantity of the product.
+#
+#     Create an object of Product and print all of it's attributes.
+def problem3():
+
+    class Product:
+        def __init__(self, name, price, quantity):
+            self.name = name
+            self.price = price
+            self.quantity = quantity
+
+        def name_change(self):
+            newname = self.name
+
+        def selfchange(self):
+            newprice = self.price
+            newquantity = self.quantity
+
+    store = Product("Spoon", "$1.00", "100")
+    print(store.name_change())
+    print(store.selfchange())
+
+def main():
+    # problem1()
+    # problem2()
+    problem3()
+main()
